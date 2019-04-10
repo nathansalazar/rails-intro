98a8ce77c4fc7a0fdcfe3ccef0abe01ba0d9927f
Nathan Salazar <nathan.salazar20@gmail.com>
2019-04-09 21:31:02 -0500

    end Ch. 15

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,7 +1,11 @@
 require "application_system_test_case"
 
 class OrdersTest < ApplicationSystemTestCase
+  include ActiveJob::TestHelper
   test "check routing number" do
+    LineItem.delete_all
+    Order.delete_all
+
     visit store_index_url
     
     first('.catalog li').click_on 'Add to Cart'
@@ -17,6 +21,26 @@ class OrdersTest < ApplicationSystemTestCase
     select 'Check', from: 'pay_type'
     
     assert_selector "#order_routing_number"
+    fill_in 'Routing #', with: '123456'
+    fill_in 'Account #', with: '987654'
+
+    perfom_enqueued_jobs do
+      click_button 'Place Order'
+    end
+
+    orders = Order.all
+    assert_equal 1, orders.size
+    order = orders.first
+    assert_equal "Dave Thomas", order.name
+    assert_equal "123 Main Street", order.address
+    assert_equal "dave@example.com", order.email
+    assert_equal "Check", order.pay_type
+    assert_equal 1, order.line_items.size
+
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ["dave@example.com"], mail.to
+    assert_equal 'Sam Ruby <depot@example.com>', mail[:from].value
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
   end
   
   setup do
@@ -28,7 +52,7 @@ class OrdersTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Orders"
   end
 
-  test "creating a Order" do
+  test "creating an Order" do
     visit orders_url
     click_on "New Order"
 
@@ -42,7 +66,7 @@ class OrdersTest < ApplicationSystemTestCase
     click_on "Back"
   end
 
-  test "updating a Order" do
+  test "updating an Order" do
     visit orders_url
     click_on "Edit", match: :first
 
@@ -56,7 +80,7 @@ class OrdersTest < ApplicationSystemTestCase
     click_on "Back"
   end
 
-  test "destroying a Order" do
+  test "destroying an Order" do
     visit orders_url
     page.accept_confirm do
       click_on "Destroy", match: :first
